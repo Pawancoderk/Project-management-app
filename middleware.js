742f615d55c6f2e727d1b0f15c23d2dc6ef8cb73
Pawan <imrealpawan@gmail.com>
2025-09-01 17:49:40 +0530

    update dependencies and add new packages for improved functionality
    - Added new dependencies: @clerk/nextjs, @clerk/themes, multiple components
    - Updated devDependencies: added tw-animate-css and ensured eslint and tailwindcss versions are consistent.

---
+++ middleware.js
@@ -0,0 +1,26 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isProtectedRoute = createRouteMatcher([
+  "/onboarding/:path*",
+  "/organization/:path*", 
+  "/project/:path*",
+  "/issue/:path*",
+]);
+
+export default clerkMiddleware(async (auth, req) => {
+  // If user is not signed in and trying to access protected route
+  const { userId, redirectToSignIn } = await auth();
+  
+  if (!userId && isProtectedRoute(req)) {
+    return redirectToSignIn();
+  }
+});
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
+};
